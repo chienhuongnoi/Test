21196584ca66da6ab11865f327194dd783fe02b6
chienhuongnoi <nvc2004vp2@gmail.com>
2025-11-08 09:21:06 +0700

    init project

---
+++ MyApp/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private List<SinhVien> list = new List<SinhVien>();
+        public TestController()
+        {
+            list.Add(new SinhVien
+            {
+                maSV = "s01",
+                tenSV = "Chien",
+                tuoi = 18
+            });
+        }
+        [HttpGet("GetAllSV")]
+        public  IActionResult getAllSV()
+        {
+            return Ok(list);
+        }
+    }
+}
---
+++ MyApp/SinhVien.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp
+{
+    public class SinhVien
+    {
+        public string maSV { get; set; }
+        public string tenSV { get; set; }
+        public int tuoi { get; set; }
+    }
+}
